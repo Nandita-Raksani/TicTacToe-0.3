78426952533d1eef0ec1b4c81ffff4a28458d7cf
1132008 <nrak15@gmail.com>
2020-05-11 22:50:55 +0530

    test: Extract in to reusable method

--- src/test/Tile.test.js
+++ src/test/Tile.test.js
@@ -20,20 +20,24 @@ describe(("<Tile/> component"), () => {
 });
 
 describe(("<Tile/> component functionality"), () => {
+    let wrapper;
+    
+    beforeEach(() => {
+        wrapper = shallow(<Tile value={Constants.PLAYER_X} onClick={jest.fn()} />);
+    });
+
     it("should display symbol X when value passed from Game is X", () => {
-        const wrapper = shallow(<Tile value={Constants.PLAYER_X} onClick={jest.fn()} />);
         expect(wrapper.find('button').props()[Constants.DATA_SYMBOL_COLOR]).toBe(Constants.EXPECT_PLAYER_X);
         expect(wrapper.find("button").text()).toEqual(Constants.EXPECT_PLAYER_X);
     });
 
     it("should display symbol O when value passed from Game is O", () => {
-        const wrapper = shallow(<Tile value={Constants.PLAYER_O} onClick={jest.fn()} />);
+        wrapper = shallow(<Tile value={Constants.PLAYER_O} onClick={jest.fn()} />);
         expect(wrapper.find('button').props()[Constants.DATA_SYMBOL_COLOR]).toBe(Constants.EXPECT_PLAYER_O);
         expect(wrapper.find("button").text()).toEqual(Constants.EXPECT_PLAYER_O);
     });
 
     it("should not allow player to play on played tile", () => {
-        const wrapper = shallow(<Tile value={Constants.PLAYER_X} onClick={jest.fn()} />);
         expect(wrapper.find('button').props()[Constants.DISABLED]).toBeTruthy();
     });
 });
